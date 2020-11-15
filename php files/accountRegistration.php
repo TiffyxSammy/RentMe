a5d09c099d8029bfa82a9d672370408168130f64
Tiffany Nguyen <53809720+7iffvny@users.noreply.github.com>
2020-11-15 15:57:07 -0500

    Create accountRegistration.php

---
+++ php files/accountRegistration.php
@@ -0,0 +1,49 @@
+<?php
+
+require('database.php');
+
+$query = 'SELECT * FROM accounts ORDER BY accountID';
+$statement = $db->prepare($query);
+$statement->execute();
+$accounts = $statement->fetchAll();
+$statement->closeCursor();
+
+?>
+
+<title>Create an Account</title>
+<link rel="stylesheet" type="text/css" href="aR.css">
+
+<body>
+
+    <h1> Account Sign Up </h1>
+
+</body>
+
+<main>
+
+    <h1> Sign Up </h1>
+
+    <form action="addAccount.php" method="post" id="add_account_form">
+
+        <label> Username: </label>
+        <input type="text" name="username"><br>
+        <label> Password: </label>
+        <input type="text" name="password"><br>
+
+        <label> First Name: </label>
+        <input type="text" name="firstName"><br>
+        <label> Last Name: </label>
+        <input type="text" name="lastName"><br>
+
+        <label> Email Address: </label>
+        <input type="text" name="emailAddress"><br>
+        <label> Credit Card: </label>
+        <input type="text" name="creditCard"><br>
+
+        <input id="submit" type="submit" value="Sign Up">
+
+
+    </form>
+
+
+</main>
