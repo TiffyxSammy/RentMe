713c8570dec0f70ee8f86dc471e9ec5ee1b57567
Tiffany Nguyen <53809720+7iffvny@users.noreply.github.com>
2020-11-15 22:55:16 -0500

    edited accountRegistration.php

--- php files/accountRegistration.php
+++ php files/accountRegistration.php
@@ -15,13 +15,38 @@ $statement->closeCursor();
 
 <body>
 
-    <h1> Account Sign Up </h1>
+
+    <nav>
+        <!--logo--------------->
+        <a href="index.html" class="logo">
+            <img src="favicon.jpg"/>
+        </a>
+        <!--menu--btn----------------->
+        <input type="checkbox" class="menu-btn" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn">
+            <span class="nav-icon"></span>
+        </label>
+        <!--menu-------------->
+        <ul class="menu">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="movies.html">Movies</a></li>
+            <li><a href="tv_shows.html">TV Shows</a></li>
+            <li><a href="favorites.html">Favorites</a></li>
+            <li><a href="#">Cart</a></li>
+        </ul>
+        <!--search------------->
+        <div class="search">
+            <input type="text" placeholder="Search" />
+            <!--search-icon----------->
+            <i class="fas fa-search"></i>
+        </div>
+    </nav>
 
 </body>
 
 <main>
-
-    <h1> Sign Up </h1>
+<div class="signup">
+    <h1> Sign up for an account today! </h1>
 
     <form action="addAccount.php" method="post" id="add_account_form">
 
@@ -29,21 +54,17 @@ $statement->closeCursor();
         <input type="text" name="username"><br>
         <label> Password: </label>
         <input type="text" name="password"><br>
-
         <label> First Name: </label>
         <input type="text" name="firstName"><br>
         <label> Last Name: </label>
         <input type="text" name="lastName"><br>
-
         <label> Email Address: </label>
         <input type="text" name="emailAddress"><br>
-        <label> Credit Card: </label>
-        <input type="text" name="creditCard"><br>
 
-        <input id="submit" type="submit" value="Sign Up">
 
+        <button id ="submit" type="submit" value="Sign Up">Sign Up</button>
 
     </form>
-
+</div>
 
 </main>
