d7ee79611e287fd54eafae462ebd3b1f11523b02
stran11 <64294836+stran11@users.noreply.github.com>
2020-11-22 14:42:27 -0500

    Update tv_shows.php

--- tv_shows.php
+++ tv_shows.php
@@ -24,7 +24,7 @@ include('database.php');
     <!--navigation-------------->
     <nav>
         <!--logo--------------->
-        <a href="index.html" class="logo">
+        <a href="index.php" class="logo">
             <img src="images/favicon.jpg" />
         </a>
         <!--menu--btn----------------->
@@ -37,7 +37,6 @@ include('database.php');
             <li><a href="index.php">Home</a></li>
             <li><a href="movies.php">Movies</a></li>
             <li><a href="tv_shows.php">TV Shows</a></li>
-            <li><a href="favorites.html">Premium</a></li>
             <li><a href="cart.php">Cart</a></li>
             <li><a href="userAccount.php">Account</a></li>
         </ul>
